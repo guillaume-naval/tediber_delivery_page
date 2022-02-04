f54349477aebb185f63a7eb3af21b382426072e4
Guillaume NAVAL <navalguillaume@gmail.com>
2022-02-04 10:20:02 +0100

    init Project

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,18 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <AppHeader />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import AppHeader from "@/components/AppHeader.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    AppHeader,
+  },
+};
 </script>
 
 <style lang="scss">
